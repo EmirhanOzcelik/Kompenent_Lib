ade8c2c530321f484f95c04a730f7ec86a7dd253
emirhan_ozclk <94894644+EmirhanOzcelik@users.noreply.github.com>
2025-05-15 14:04:04 +0300

    Delete BUT_POT_LIB/src directory

--- BUT_POT_LIB/src/BUTPOTLIB.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Arduino.h"
-
-#define SERI_TEST 0
-
-extern uint16_t max_deger_yakala(uint16_t giris, uint16_t maxx = UINT16_MAX);
-extern void max_deger_elle_gir(uint16_t veri);
--- BUT_POT_LIB/src/Kompenent.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "ButnLiBb.h"
-#include "PotLiBb.h"
-#include "BUTPOTLIB.h"
